8deeb42653b81c84687c0fb02b0991f85728a0c7
Asplund-chasacademy <andreas.asplund@chasacademy.se>
2025-09-28 13:35:49 +0200

    Kopplat CI till ACR

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+const port = process.env.WEBSITES_PORT || process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Docker image pushed to ACR!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
